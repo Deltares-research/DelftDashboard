eb73db9943013e54380a095a9a31188c33a80117
luisrodriguezgalvez <luis.rodriguez@deltares.nl>
2023-06-15 10:18:27 +0200

    fix errors in the exposure asset locations tab
    including variables in the fiat group, removing unnecessary methods, and creating placeholder ones when needed

--- src/delftdashboard/models/fiat/exposure_locations.py
+++ src/delftdashboard/models/fiat/exposure_locations.py
@@ -18,8 +18,32 @@ def set_variables(*args):
     app.model["fiat"].set_input_variables()
 
 
-def set_asset_locations(*args):
-    print("Set asset locations")
+# def set_asset_locations(*args):
+#     print("Set asset locations")
     
 def set_asset_locations_field(*args):
     app.model["fiat"].set_asset_locations_field()
+
+
+def display_area_geometry(*args):
+    print("Display area")
+
+
+def create_nsi_assets(*args):
+    print("Create NSI assets")
+
+
+def draw_area_interest(*args):
+    print("Draw area of interest")
+
+
+def display_asset_locations(*args):
+    print("Display assets")
+
+
+def display_extraction_method(*args):
+    print("Display extraction method")
+
+
+def draw_extraction_method_exception(*args):
+    print("Draw extraction method")
