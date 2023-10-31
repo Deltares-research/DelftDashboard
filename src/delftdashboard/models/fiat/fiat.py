37ad197e37ed52973c6f6a58db05c7d06efff652
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2023-10-31 17:32:11 +0100

    few fixes

--- src/delftdashboard/models/fiat/fiat.py
+++ src/delftdashboard/models/fiat/fiat.py
@@ -337,7 +337,7 @@ class Model(GenericModel):
         if not okay:
             return
 
-    def specify_damage_curves(self):
+    def damage_curves_specify(self):
         # get window config yaml path
         pop_win_config_path = str(
             Path(
@@ -348,6 +348,10 @@ class Model(GenericModel):
             / "config"
             / "vulnerability_specify_damage_curves.yml"
         )
+        # Create pop-up and only continue if user presses ok
+        okay, data = app.gui.popup(pop_win_config_path, None)
+        if not okay:
+            return
 
     def classification_standarize(self):
         # get window config yaml path
@@ -360,7 +364,6 @@ class Model(GenericModel):
             / "config"
             / "exposure_classification_standarize.yml"
         )
-
         # Create pop-up and only continue if user presses ok
         okay, data = app.gui.popup(pop_win_config_path, None)
         if not okay:
