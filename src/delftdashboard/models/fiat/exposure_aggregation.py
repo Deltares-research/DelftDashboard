2606aed9e7064039624e006c42300a7f1ceacacc
sarah.rautenbach <sarah.rautenbach@deltares.nl>
2023-11-03 08:03:56 +0100

    add print aggregation command

--- src/delftdashboard/models/fiat/exposure_aggregation.py
+++ src/delftdashboard/models/fiat/exposure_aggregation.py
@@ -118,7 +118,7 @@ def add_aggregations(*args):
     attribute   = aggregation_table["Aggregation Attribute"].tolist()
     label = aggregation_table["Aggregation Label"].tolist()
     app.model["fiat"].domain.exposure_vm.set_aggregation_areas_config(fn, attribute, label)
-
+    print("Aggregation zones added")
     #attribute_to_visualize = "TO FILL"
     #data_to_visualize = "TO FILL"
     #gdf = gpd.read_file(data_to_visualize)
