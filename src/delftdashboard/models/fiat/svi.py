dcf0c9a3e00bf7c7ae22a40bde8959ad95a75ee9
Frederique <frederique.degroen@deltares.nl>
2023-12-08 15:44:10 +0100

    Updates according to changes in hydromt-fiat

--- src/delftdashboard/models/fiat/svi.py
+++ src/delftdashboard/models/fiat/svi.py
@@ -16,18 +16,16 @@ def add_svi(*args):
 
     # Get the variables
     census_key = "495a349ce22bdb1294b378fb199e4f27e57471a9"  ## TODO: CHANGE TO INPUT
-    state_abbreviation = "SC"  ## TODO: CHANGE TO INPUT
     year_data = 2021  ## TODO: CHANGE TO INPUT
-    county = "019"  ## TODO: CHANGE TO INPUT
 
     if app.gui.getvar(group, "use_svi"):
         app.active_model.domain.svi_vm.set_svi_settings(
-            census_key, state_abbreviation, year_data, county
+            census_key, year_data
         )
     
     if app.gui.getvar(group, "use_equity"):
         app.active_model.domain.svi_vm.set_equity_settings(
-            census_key, state_abbreviation, year_data, county
+            census_key, year_data
         )
     
     if app.gui.getvar(group, "use_svi") or app.gui.getvar(group, "use_equity"):
