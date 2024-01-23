cbc84e4e57f1230faea9a36354210d43779fbfbb
Daley Adrichem <Daley.Adrichem@deltares.nl>
2024-01-23 16:04:46 +0100

    Add legend to mask layer

--- src/delftdashboard/models/sfincs_hmt/domain.py
+++ src/delftdashboard/models/sfincs_hmt/domain.py
@@ -7,8 +7,6 @@ def select(*args):
     # Show the grid and mask
     app.map.layer["sfincs_hmt"].layer["grid"].activate()
     app.map.layer["sfincs_hmt"].layer["mask_active"].activate()
-    app.map.layer["sfincs_hmt"].layer["mask_bound_wlev"].activate()
-    app.map.layer["sfincs_hmt"].layer["mask_bound_outflow"].activate()
 
 
 def set_variables(*args):
