bcae3eddfc6504e45347606e617a3eb75f513075
sarah.rautenbach <sarah.rautenbach@deltares.nl>
2024-01-29 15:28:02 +0100

    update term method to join_method

--- src/delftdashboard/models/fiat/exposure_finished_floor_height.py
+++ src/delftdashboard/models/fiat/exposure_finished_floor_height.py
@@ -107,7 +107,7 @@ def add_to_model(*args):
     app.active_model.domain.exposure_vm.set_ground_floor_height(
         source=source_path,
         attribute_name=attribute_name_gfh,
-        method=method_gfh,
+        join_method=method_gfh,
         max_dist=max_dist_gfh,
         )
 
