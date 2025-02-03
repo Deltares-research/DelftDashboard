b350cb36fc423f35ec8e9db436b78af3b18ef57f
maartenvanormondt <82946105+maartenvanormondt@users.noreply.github.com>
2025-02-03 14:09:06 -0500

    removed comment lines from delftdashboard.bat

--- src/delftdashboard/delftdashboard.bat
+++ src/delftdashboard/delftdashboard.bat
@@ -1,10 +1,8 @@
 @echo OFF
 set CONDAPATH=c:\work\miniconda3
-set DDBPATH=c:\work\checkouts\git\DelftDashboard\src\delftdashboard
 set ENVNAME=delftdashboard
 
 rem The following command activates the base environment.
-rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
 if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
 
 rem Activate the conda environment
@@ -15,16 +13,7 @@ rem Run a python script in that environment
 echo import delftdashboard > _start_ddb.py
 echo delftdashboard.start() >> _start_ddb.py
 python _start_ddb.py
-rem python %DDBPATH%\start_delftdashboard.py
 
 rem Deactivate the environment
 call conda deactivate
 del _start_ddb.py
-
-rem If conda is directly available from the command line then the following code works.
-rem call activate someenv
-rem python script.py
-rem conda deactivate
-
-rem One could also use the conda run command
-rem conda run -n someenv python script.py
