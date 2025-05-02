5aa87762147c1a4812a802b8ac22a170ddb5bfaa
roelvink <floortje.roelvink@deltares.nl>
2025-05-02 14:24:22 +0200

    added view menu, observation lines, correct time format,  correction observation points

--- src/delftdashboard/models/delft3dfm/delft3dfm.py
+++ src/delftdashboard/models/delft3dfm/delft3dfm.py
@@ -14,6 +14,8 @@ from delftdashboard.app import app
 
 # from cht_delft3dfm.delft3dfm import Delft3DFM
 from cht_delft3dfm import Delft3DFM
+import datetime
+import ast
 
 class Model(GenericModel):
     def __init__(self, name):
@@ -28,9 +30,35 @@ class Model(GenericModel):
         self.domain.fname = 'flow.mdu'
         self.set_gui_variables()
         self.observation_points_changed = False
+        self.observation_lines_changed = False
         self.discharge_points_changed = False
         self.boundaries_changed = False
         self.thin_dams_changed = False
+
+    def get_view_menu(self):
+        model_view_menu = {}
+        model_view_menu["text"] = self.long_name
+        model_view_menu["menu"] = []
+        model_view_menu["menu"].append({"variable_group": self.name,
+                                        "id": f"view.{self.name}.grid",
+                                        "text": "Grid",
+                                        "variable": "view_grid",
+                                        "separator": True,
+                                        "checkable": True,
+                                        "method": self.set_view_menu,
+                                        "option": "grid",
+                                        "dependency": [{"action": "check", "checkfor": "all", "check": [{"variable": "view_grid", "operator": "eq", "value": True}]}]})
+        return model_view_menu
+
+    def set_view_menu(self, option, checked):
+        if option == "grid":
+            print(f"Checked: {checked}")
+            if app.gui.getvar(self.name, "view_grid"):
+                app.map.layer["delft3dfm"].layer["grid"].show()
+                print("Grid is made visible")
+            else:
+                app.map.layer["delft3dfm"].layer["grid"].hide()
+                print("Grid is made invisible")
 
     def add_layers(self):
         # Add main DDB layer
@@ -80,6 +108,19 @@ class Model(GenericModel):
                         line_color_selected="white",
                         fill_color_selected="red")
 
+        from .observation_points_crs import obs_lines_created
+        from .observation_points_crs import obs_lines_selected
+        from .observation_points_crs import obs_lines_modified
+        layer.add_layer("observation_lines",
+                            type="draw",
+                            shape="polyline",
+                            create=obs_lines_created,
+                            modify=obs_lines_modified,
+                            select=obs_lines_selected,
+                            polyline_line_color="yellow",
+                            polyline_line_width=2.0,
+                            polyline_line_opacity=1.0)
+        
         # from .observation_points_spectra import select_observation_point_from_map_spectra
         # layer.add_layer("observation_points_spectra",
         #                 type="circle_selector",
@@ -104,6 +145,7 @@ class Model(GenericModel):
             app.map.layer["delft3dfm"].layer["boundary_line"].deactivate()
             # Observation points are made grey
             app.map.layer["delft3dfm"].layer["observation_points"].deactivate()
+            app.map.layer["delft3dfm"].layer["observation_lines"].deactivate()
             # app.map.layer["delft3dfm"].layer["observation_points_spectra"].deactivate()
         elif mode == "invisible":
             # Everything set to invisible
@@ -119,7 +161,7 @@ class Model(GenericModel):
 
     def set_gui_variables(self):
         group = "delft3dfm"
-        subgroups = ['numerics', 'physics', 'time', 'output', 'geometry','wind',]
+        subgroups = ['numerics', 'physics', 'output', 'geometry','wind',]
                      
         # Input variables
         for groupname in subgroups:
@@ -145,6 +187,14 @@ class Model(GenericModel):
                     #     var_value = str(var_value)
                     app.gui.setvar(group, f'{groupname}.{var_name}', var_value)
 
+        # Overwrite time variables to set them to datetime objects
+        tnow = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+        app.gui.setvar(group, "time.refdate", tnow)
+        app.gui.setvar(group, "time.tstart", tnow)
+        app.gui.setvar(group, "time.tstop", tnow + datetime.timedelta(days=1))
+
         app.gui.setvar(group, "setup.x0", 0)
         app.gui.setvar(group, "setup.y0", 1)
         app.gui.setvar(group, "setup.nmax", 0)
@@ -164,12 +214,30 @@ class Model(GenericModel):
         app.gui.setvar(group, "nr_observation_points", 0)
         app.gui.setvar(group, "active_observation_point", 0)
 
-        app.gui.setvar(group, "observation_point_names_spectra", [])
-        app.gui.setvar(group, "nr_observation_points_spectra", 0)
-        app.gui.setvar(group, "active_observation_point_spectra", 0)
+        app.gui.setvar(group, "observation_line_names", [])
+        app.gui.setvar(group, "nr_observation_lines", 0)
+        app.gui.setvar(group, "active_observation_line", 0)
+        app.gui.setvar(group, "observation_line_index", 0)
 
-        app.gui.setvar(group, "observation_point_names_crs", [])
-        app.gui.setvar(group, "active_observation_point_crs", 0)
+        app.gui.setvar(group, "boundary_line_active", 0)
+        app.gui.setvar(group, "boundary_conditions_tide_model", [])
+
+        # Boundary conditions
+        # app.gui.setvar(group, "boundary_point_names", [])
+        # app.gui.setvar(group, "nr_boundary_points", 0)
+        # app.gui.setvar(group, "active_boundary_point", 0)
+        # app.gui.setvar(group, "boundary_dx", 10000.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_shape", "constant")
+        app.gui.setvar(group, "boundary_conditions_timeseries_time_step", 600.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_offset", 0.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_offset_custom", 0.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_amplitude", 1.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_phase", 0.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_period", 43200.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_peak", 1.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_tpeak", 86400.0)
+        app.gui.setvar(group, "boundary_conditions_timeseries_duration", 43200.0)
+        # app.gui.setvar(group, "boundary_conditions_tide_model", [])
 
     # def set_model_variables(self, varid=None, value=None):
     #     # Copies gui variables to delft3dfm input variables
@@ -189,7 +257,7 @@ class Model(GenericModel):
 
     def set_input_variables(self):
         # Update all model input variables
-        subgroups = ['numerics', 'physics', 'time', 'output', 'geometry', 'wind', ]
+        subgroups = ['numerics', 'physics', 'geometry', 'wind', ]
 
         for groupname in subgroups:
             subgroup = getattr(self.domain.input, groupname)
@@ -214,7 +282,36 @@ class Model(GenericModel):
                     if 'filepath' in var_name.lower() and value is not None:  # Check if it's a filepath
                         value = Path(value)  # Parse to Path
                     setattr(subgroup, var_name, value)
-                
+        # Set timeframe of domain using set_timeframe
+        self.set_timeframe()
+        
+        # Set output variables
+        hisinterval = app.gui.getvar("delft3dfm", "output.hisinterval")
+        if isinstance(hisinterval, str):
+            hisinterval = ast.literal_eval(hisinterval)
+        setattr(self.domain.input.output, "hisinterval", hisinterval)
+        mapinterval = app.gui.getvar("delft3dfm", "output.mapinterval")
+        if isinstance(mapinterval, str):
+            mapinterval = ast.literal_eval(mapinterval)
+        setattr(self.domain.input.output, "mapinterval", mapinterval)
+        rstinterval = app.gui.getvar("delft3dfm", "output.rstinterval")
+        if isinstance(rstinterval, str):
+            rstinterval = ast.literal_eval(rstinterval)
+        setattr(self.domain.input.output, "rstinterval", rstinterval)
+
+    def set_timeframe(self):
+        refdate = app.gui.getvar("delft3dfm", "time.refdate")
+        refdate = refdate.strftime("%Y%m%d")
+        setattr(self.domain.input.time, "refdate", refdate)
+
+        start_time = app.gui.getvar("delft3dfm", "time.tstart")
+        start_time = start_time.strftime("%Y%m%d%H%M%S")
+        setattr(self.domain.input.time, "startdatetime", start_time)
+
+        stop_time = app.gui.getvar("delft3dfm", "time.tstop")
+        stop_time = stop_time.strftime("%Y%m%d%H%M%S")
+        setattr(self.domain.input.time, "stopdatetime", stop_time)
+     
     def open(self):
         # Open input file, and change working directory
         fname = app.gui.window.dialog_open_file("Open file", filter="Delft3D-FM input file (flow.mdu)")
@@ -236,12 +333,11 @@ class Model(GenericModel):
     def save(self):
         # Write flow.mdu
         self.domain.path = os.getcwd()
-        self.domain.write_input_file(input_file=self.domain.fname)
+        self.domain.write_input_file(input_file=os.path.join(self.domain.path, self.domain.fname))        
         # self.domain.write_batch_file()
 
-
     def load(self):
-        self.domain.read()
+        self.domain.read_input_file(input_file=os.path.join(self.domain.path, self.domain.fname))
 
     def plot(self):
         # Grid
@@ -252,6 +348,12 @@ class Model(GenericModel):
         # Boundary points
         app.map.layer["delft3dfm"].layer["boundary_line"].clear()
         app.map.layer["delft3dfm"].layer["boundary_line"].set_data(self.domain.boundary_conditions.gdf)
+
+        # Observation points
+        app.map.layer["delft3dfm"].layer["observation_points"].set_data(app.model["delft3dfm"].domain.observation_point_gdf, 0)
+
+        # Observation cross sections
+        # app.map.layer["delft3dfm"].layer["observation_lines"].set_data(app.model["delft3dfm"].domain.observation_line_gdf)
 
         # # Boundary points
         # gdf = self.domain.boundary_conditions.gdf
