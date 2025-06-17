04b06cbca0f9c0409fdc6f614de832596cc3c8df
roelvink <floortje.roelvink@deltares.nl>
2025-06-17 15:39:59 +0100

    add boundary polygon name and resolution for boundary points

--- src/delftdashboard/toolboxes/modelmaker_delft3dfm/boundary.py
+++ src/delftdashboard/toolboxes/modelmaker_delft3dfm/boundary.py
@@ -30,9 +30,19 @@ def delete_open_boundary_polygon(*args):
     if len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon) == 0:
         return
     index = app.gui.getvar("modelmaker_delft3dfm", "open_boundary_polygon_index")
+    app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon_names.pop(index)
+    feature_id = app.map.layer["modelmaker_delft3dfm"].layer["open_boundary_polygon"].get_feature_id(index)
     # Delete from map
-    gdf = app.map.layer["modelmaker_delft3dfm"].layer["open_boundary_polygon"].delete_feature(index)
-    app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon = gdf
+    gdf = app.map.layer["modelmaker_delft3dfm"].layer["open_boundary_polygon"].delete_feature(feature_id)
+    # Delete from app
+    app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon = app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon.drop(index)
+    if len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon) > 0:
+        app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon = app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon.reset_index(drop=True)
+    # If the last polygon was deleted, set index to last available polygon
+    if index > len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon) - 1:
+        index = max(len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon) - 1, 0)
+        app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_index", index)
+    # app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon = gdf
     update()
 
 def load_open_boundary_polygon(*args):
@@ -60,9 +70,21 @@ def select_open_boundary_polygon(*args):
     app.map.layer["modelmaker_delft3dfm"].layer["open_boundary_polygon"].activate_feature(index)
 
 def open_boundary_polygon_created(gdf, index, id):
+    while True:
+        name, okay = app.gui.window.dialog_string("Edit name for new open boundary polygon")
+        if not okay:
+            app.map.layer["modelmaker_delft3dfm"].layer["open_boundary_polygon"].delete_feature(index)
+            return            # Cancel was clicked
+        if name in app.gui.getvar("modelmaker_delft3dfm", "open_boundary_polygon_names"):
+            app.gui.window.dialog_info("An open boundary polygon with this name already exists !")
+        else:
+            break
+    gdf.at[index, "name"] = name
     app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon = gdf
     nrp = len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon)
     app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_index", nrp - 1)
+    app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon_names.append(name)
+
     update()
 
 def open_boundary_polygon_modified(gdf, index, id):
@@ -72,6 +94,11 @@ def open_boundary_polygon_selected(index):
     app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_index", index)
     update()
 
+# def edit_resolution(*args):
+#     # Get index of selected polygon 
+#     index = app.gui.getvar("modelmaker_delft3dfm", "refinement_polygon_index")
+#     app.toolbox["modelmaker_delft3dfm"].refinement_polygon.at[index, "min_edge_size"] = app.gui.getvar("modelmaker_delft3dfm", "ref_min_edge_size")
+#     update()
 
 # def draw_outflow_boundary_polygon(*args):
 #     app.map.layer["modelmaker_delft3dfm"].layer["outflow_boundary_polygon"].crs = app.crs
@@ -118,13 +145,11 @@ def open_boundary_polygon_selected(index):
 
 def update():
     # Open
-    nrp = len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon)
-    names = []
-    for ip in range(nrp):
-        names.append(str(ip + 1))
-    app.gui.setvar("modelmaker_delft3dfm", "nr_open_boundary_polygons", nrp)
-    app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_names", names)
     index = app.gui.getvar("modelmaker_delft3dfm", "open_boundary_polygon_index")
+    nrp = len(app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon)
+    polnames = app.toolbox["modelmaker_delft3dfm"].open_boundary_polygon_names
+    app.gui.setvar("modelmaker_delft3dfm", "nr_open_boundary_polygons", nrp)
+    app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_names", polnames)
     if index > nrp - 1:
         index = max(nrp - 1, 0)
     app.gui.setvar("modelmaker_delft3dfm", "open_boundary_polygon_index", index)
@@ -157,6 +182,8 @@ def load_bnd(*args):
                                     allow_directory_change=True)
     app.toolbox["modelmaker_delft3dfm"].load_bnd(fname[0])
     
+def set_resolution(*args):
+    update()
 # def update_mask(*args):
 #     app.toolbox["modelmaker_delft3dfm"].update_mask()
 
