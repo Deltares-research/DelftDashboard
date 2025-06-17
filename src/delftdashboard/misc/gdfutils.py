16cac1a37dd28278513e46ef5fe6dfbb78c4656b
roelvink <floortje.roelvink@deltares.nl>
2025-06-17 15:36:15 +0100

    add mline2point (for fm boundary)

--- src/delftdashboard/misc/gdfutils.py
+++ src/delftdashboard/misc/gdfutils.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import pandas as pd
+from shapely.geometry import Point
 
 def mpol2pol(gdf):
     """
@@ -21,3 +22,32 @@ def mpol2pol(gdf):
             gdfout = pd.concat([gdfout, gdftmp])
     gdfout.set_crs(gdf.crs)        
     return gdfout
+
+def mline2point(gdf):
+    """
+    Convert (Multi)LineString to Point
+    """
+
+    point_records = []
+
+    for idx, row in gdf.iterrows():
+        geom = row.geometry
+        base_name  = row['name']  # Preserve the 'name' column
+
+        if geom.geom_type == 'LineString':
+            coords = geom.coords
+        elif geom.geom_type == 'MultiLineString':
+            coords = []
+            for part in geom.geoms:
+                coords.extend(part.coords)
+        else:
+            continue  # Skip unsupported geometry types
+
+        for i, coord in enumerate(coords, start=1):
+            new_name = f"{base_name}_{str(i).zfill(4)}"
+            point_records.append({'geometry': Point(coord), 'name': new_name})
+
+    # Create new GeoDataFrame
+    gdf_points = gpd.GeoDataFrame(point_records, crs=gdf.crs)
+
+    return gdf_points
